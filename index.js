8ceb1722fb5bc35b51e9b4514060e8a75acb6ec8
Siddhant Singh <siddhantsingh@Siddhants-MacBook-Pro.local>
2023-06-07 19:06:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+const request = require('request')
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+})
+
+app.get('/cors', (req, res) => {
+  request(
+    { url: 'https://connect.realtime.li/realtime' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message:
+      err.message })
+    }
+    
+    res.set('Content-Type', 'application/rss+xml')
+    res.send(Buffer.from(body))
+  })
+})
+app.listen(process.env.PORT || 3000)
