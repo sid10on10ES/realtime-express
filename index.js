98fb3a41c2c63b641cc6a7fbc88053937681d2c7
Siddhant Singh <siddhant.singh@essentiallysports.com>
2023-06-07 19:16:17 +0530

    added headers

--- index.js
+++ index.js
@@ -14,14 +14,17 @@ app.get('/', (req, res)=>{
 
 app.get('/cors', (req, res) => {
   request(
-    { url: 'https://connect.realtime.li/realtime' },
+    { url: 'https://connect.realtime.li/realtime', headers: {
+      'client_secret': '12d2a9a46ecdd95d48a2cda309ecb458',
+      'api_key': 'a6daa1f4f450756'
+    } },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message:
       err.message })
     }
     
-    res.set('Content-Type', 'application/rss+xml')
+    res.set('Content-Type', 'application/json')
     res.send(Buffer.from(body))
   })
 })
