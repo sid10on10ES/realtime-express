c779f6e14c009ec00e7aea25d286337ec76d1c59
Siddhant Singh <siddhant.singh@essentiallysports.com>
2023-07-02 21:08:59 +0530

    changed err response

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res)=>{
 
 
 
-app.get('/cors', (req, res) => {
+app.get('/data', (req, res) => {
   request(
     { url: 'https://connect.realtime.li/realtime', headers: {
       'client_secret': '12d2a9a46ecdd95d48a2cda309ecb458',
@@ -30,12 +30,13 @@ app.get('/cors', (req, res) => {
     } },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
+        console.log(response)
         return res.status(500).json({ type: 'error', message:
-      err.message })
+      error })
     }
     
     res.set('Content-Type', 'application/json')
     res.send(Buffer.from(body))
   })
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 8000)
